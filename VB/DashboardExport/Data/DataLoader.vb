821ee34ab9eeee5f75aa56e1e72a1c3d74ca3a2a
DevExpressExampleBot <fakeEmail>
2023-10-09 16:08:32 +0400

    Source auto update [skip ci]

--- VB/DashboardExport/Data/DataLoader.vb
+++ VB/DashboardExport/Data/DataLoader.vb
@@ -1,24 +1,24 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Collections.Generic
 Imports System.Data
 Imports System
 Imports DevExpress.Utils
 
 Namespace DashboardExport
-	Public NotInheritable Class DataLoader
-		Private Sub New()
-		End Sub
-		Private Shared Function LoadData(ByVal fileName As String) As DataSet
-			Dim path As String = FilesHelper.FindingFileName(AppDomain.CurrentDomain.BaseDirectory, String.Format("Data\{0}.xml", fileName), False)
-			Dim ds As New DataSet()
-			ds.ReadXml(path, XmlReadMode.ReadSchema)
-			Return ds
-		End Function
-		Public Shared Function LoadEmployees() As DataSet
-			Return LoadData("EmployeesAndDepartments")
-		End Function
-		Public Shared Function LoadCustomerSupport() As DataSet
-			Return LoadData("CustomerSupport")
-		End Function
-	End Class
+
+    Public Module DataLoader
+
+        Private Function LoadData(ByVal fileName As String) As DataSet
+            Dim path As String = FilesHelper.FindingFileName(AppDomain.CurrentDomain.BaseDirectory, String.Format("Data\{0}.xml", fileName), False)
+            Dim ds As DataSet = New DataSet()
+            ds.ReadXml(path, XmlReadMode.ReadSchema)
+            Return ds
+        End Function
+
+        Public Function LoadEmployees() As DataSet
+            Return LoadData("EmployeesAndDepartments")
+        End Function
+
+        Public Function LoadCustomerSupport() As DataSet
+            Return LoadData("CustomerSupport")
+        End Function
+    End Module
 End Namespace
